11cd957a1ad552c964d7b6f3d47111ed2af21c4f
anhpt6 <anhpt6@tnteco.vn>
2024-06-24 11:31:55 +0700

    update dist 123

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/App.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
