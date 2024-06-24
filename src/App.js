b3c54e9a6b30967e4b3339ff29c8b57e366916b3
anhpt6 <anhpt6@tnteco.vn>
2024-06-24 11:19:11 +0700

    update dist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import ReactDOM from 'react-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <img src="/logo192.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
